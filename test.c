7285d05c94e52bd02b9d1af220255102ab67ba84
cheong2k <cheong@particle.io>
2018-06-07 18:41:49 +0800

    Modified for print

--- test.c
+++ test.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
     char my_string[] = "Hello, world!";
 
-    printf("%s \r\n", my_string);
+    printf("%s\r\n", &my_string[7]);
+
 
     return 1;
 }
